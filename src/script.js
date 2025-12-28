5cde19dc864cd07209fe0056f38a0bf47580f5e9
Aabhas Bhandari <aabhasb25@gmail.com>
2025-12-28 18:33:06 +0545

    Inject date and weekday into DOM

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const day = document.getElementById("weekday");
+const date = document.getElementById("date");
+const add_button = document.getElementById("add_btn");
+const todo_input = document.getElementById("todo_input");
+const todo_list_UL = document.getElementById("todo_list");
+
+// Adding Day as a name
+let today = new Date();
+day.textContent = today.toLocaleString("default", { weekday: "long" });
+
+const current_date = today.getDate();
+const month = today.toLocaleString("default", { month: "short" });
+const year = today.getFullYear();
+date.textContent = `${month} ${current_date}, ${year}`;
